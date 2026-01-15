592148dfa465ecee51a262fe14347604c31803bc
Juan Camilo Pérez <juancamiloperezc@gmail.com>
2026-01-15 16:01:20 -0500

    se refactoriza interfaz del repositorio para manejar de mejor forma los tipos genericos

--- tmdb-explorer/src/domain/repositories/preferences.domain.repository.ts
+++ tmdb-explorer/src/domain/repositories/preferences.domain.repository.ts
@@ -1,7 +1,7 @@
 import type { Observable } from "../../core/observer/observable";
 import type { ThemeModel } from "../model/theme.model";
 
-export interface IPreferencesRepository <T> extends Observable<T> {
+export interface IPreferencesRepository extends Observable<ThemeModel> {
   set theme(themeModel);
   get theme(): ThemeModel;
 }
