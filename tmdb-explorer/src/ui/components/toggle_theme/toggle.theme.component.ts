2a5f604ad06a465a529c9ab58c98ede3545fe548
Juan Camilo Pérez <juancamiloperezc@gmail.com>
2026-01-15 16:04:05 -0500

    se crea componente para realizar el cambio de tema oscuro/claro

---
+++ tmdb-explorer/src/ui/components/toggle_theme/toggle.theme.component.ts
@@ -0,0 +1,65 @@
+import "./toggle-theme.css";
+
+import { Observable } from "../../../core/observer/observable"
+
+export class ToggleThemeComponent extends Observable<boolean> {
+  
+  private _isDarkTheme!: boolean;
+
+  private root: HTMLElement;
+
+  constructor(isDarkTheme: boolean){
+    super();
+  
+    this.root = document.createElement("div");
+    
+    this._isDarkTheme = isDarkTheme;
+    
+    this.init();
+    this.initEvents();
+  }
+  
+  private init() : void {
+
+    this.root.className = "toggle-theme"
+
+    this.root.innerHTML = `
+      <label for="toggle-theme-check"> 
+        <i class="toggle-theme-icon fa-solid ${this._isDarkTheme ? "fa-moon" : "fa-sun"}"> </i>
+      </label>
+      
+      <input id="toggle-theme-check" type="checkbox" ${this._isDarkTheme ? "checked" : "" }>
+    `
+  }
+
+  private initEvents() : void {
+    const toggleThemeCheckbox = this.root.querySelector<HTMLInputElement>("#toggle-theme-check");
+    
+    toggleThemeCheckbox?.addEventListener("change", (event: Event) => {
+      const checkbox = event.target as HTMLInputElement;
+      this.isDarkTheme = checkbox.checked;
+      this.notify(this._isDarkTheme);
+    });
+  }
+
+  private changeTheme() : void {
+    const currentClass = this._isDarkTheme ? "fa-sun" : "fa-moon";
+    const beforeClass = this._isDarkTheme ? "fa-moon" : "fa-sun";
+    
+    const iconTheme = this.root.querySelector<HTMLElement>(".toggle-theme-icon");
+    iconTheme?.classList.replace(currentClass, beforeClass);
+  }
+
+  public set isDarkTheme(isDarkTheme: boolean) {
+    this._isDarkTheme = isDarkTheme;
+    this.changeTheme();
+  }
+
+  public get isDarkTheme() {
+    return this._isDarkTheme;
+  }
+
+  public render(): Node {
+    return this.root;
+  }
+}
