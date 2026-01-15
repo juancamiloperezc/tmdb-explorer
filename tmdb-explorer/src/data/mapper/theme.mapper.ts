d518115098511afc25292f50900c69eed9098c2f
Juan Camilo Pérez <juancamiloperezc@gmail.com>
2026-01-15 10:05:21 -0500

    se añade mapper para convertir los modelos de datos al modelo de dominio, y viceversa

---
+++ tmdb-explorer/src/data/mapper/theme.mapper.ts
@@ -0,0 +1,14 @@
+import { ThemeModel } from "../../domain/model/theme.model";
+import type { ThemeString } from "../storage/theme.storage";
+
+export function themeStringToThemeModel(theme: ThemeString) : ThemeModel | null {
+    if(theme === "light") return ThemeModel.LIGHT;
+    if(theme === "dark") ThemeModel.DARK;
+    return null;
+}
+
+export function themeModelToThemeString(theme: ThemeModel) : ThemeString | null {
+    if(theme === ThemeModel.LIGHT) return "light";
+    if(theme === ThemeModel.DARK) return "dark";
+    return null;
+}
